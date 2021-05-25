a66315dfd122cd969abe9a6efbcb908755f5bf37
Robbi <Robbie_Bridgwater@Hotmail.co.uk>
2021-05-25 15:41:14 +0100

    updated the import for the book model to have a lowercase b

--- routes/API.js
+++ routes/API.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Book = require('../models/Book');
+const Book = require('../models/book');
 
 // GET ALL BOOKS
 router.get("/api/books", async(req, res) => {
