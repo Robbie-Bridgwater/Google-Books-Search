088922955e8d16e80d329f7ff0135dfc8f591ee8
Robbi <Robbie_Bridgwater@Hotmail.co.uk>
2021-05-25 13:45:14 +0100

    changed the id attribute of the book card to the correct method and removed nested P tag from card.text

--- client/src/components/BookCard/index.js
+++ client/src/components/BookCard/index.js
@@ -11,7 +11,7 @@ const BookCard = ({ books, saveBook }) => {
       <Row>
         <Col>
           {books.map((book) => (
-            <Card key={book._id} style={{ width: "100%", flexDirection: "row" }}>
+            <Card key={book.id} style={{ width: "100%", flexDirection: "row" }}>
               <Card.Img className='col-xs-4 col-sm-3 col-md-3 col-lg-3' variant="top" src={book.volumeInfo.imageLinks
                 ? book.volumeInfo.imageLinks.thumbnail
                 : "https://picsum.photos/200/300"
@@ -24,9 +24,7 @@ const BookCard = ({ books, saveBook }) => {
                     book.volumeInfo.authors.length > 1
                     ? book.volumeInfo.authors.join(", ")
                     : book.volumeInfo.authors}
-                  <p className="card-text">
                     <small className="text-muted">{book.volumeInfo.publishedDate}</small>
-                  </p>
                 </Card.Text>
                 <Button variant="secondary" href={book.volumeInfo.previewLink}>View on Google Books</Button>{" "}
                 <Button variant="success" onClick={() => saveBook(book)}>Save Book</Button>
